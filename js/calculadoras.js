63a2f68587f06138cb094f7eb1ea30b789a321d3
Allan Ched <allanched113@gmail.com>
2022-02-07 13:11:24 -0300

    atualizacao calculadora

--- js/calculadoras.js
+++ js/calculadoras.js
@@ -1,6 +1,6 @@
 function calcular1(n1, n2){
-    n1 = parseFloat (document.getElementById("n1").value)
-    n2 = parseFloat(document.getElementById("n2").value)
+    n1 = Number(document.getElementById("n1").value)
+    n2 = Number(document.getElementById("n2").value)
 
     selector = document.getElementById("selector").value
     
@@ -32,9 +32,17 @@ function calcular1(n1, n2){
 }
 
 function calcular2(n1, n2){
-    n1 = parseFloat (document.getElementById("n1").value)
-    n2 = parseFloat(document.getElementById("n2").value)
+    n1 = Number(document.getElementById("n1").value)
+    n2 = Number(document.getElementById("n2").value)
 
     calculo = (n1 * n2 / 100)
     document.getElementById("resultado").innerHTML = `${n1}% de ${n2} é = ${calculo}`
+}
+
+function calcular3(n1, n2){
+    n1 = Number(document.getElementById("n1").value)
+    n2 = Number(document.getElementById("n2").value)
+
+    calculo = (n1 ** n2)
+    document.getElementById("resultado").innerHTML = `O resultado da potência é = ${calculo}`
 }
